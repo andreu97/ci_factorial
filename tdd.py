47eb9a0e1e68313022f43c753241e56507cca5b3
andreu97 <albors.marques.andreu@gmail.com>
2024-02-18 18:40:26 +0100

    factorial

--- tdd.py
+++ tdd.py
@@ -1,11 +1,18 @@
+"""calcul factorial"""
 def factorial(e):
+    """
+    funcio per al factorial
+    
+    entra: 
+        int: num
+    ixida:
+        int: torna el factorial del num donat
+    
+    """
     if not isinstance(e,int):
         raise TypeError("Error, no has ficat un numero")
     if e==1:
         return 1
     if e<0:
         raise ValueError("Error numero negatiu")
-    else:
-        return e * factorial(e-1)
-    
-    
+    return e * factorial(e-1)
