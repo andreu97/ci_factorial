3306b57a6745934efa87e31891b416464c316d76
andreu97 <74356521+andreu97@users.noreply.github.com>
2024-02-18 18:23:53 +0100

    Add files via upload

---
+++ practica3/test/test_tdd.py
@@ -0,0 +1,17 @@
+import pytest
+
+from practiques.practica3.src.tdd import factorial
+
+def test_tdd():
+    assert factorial(1)==1
+    
+def test_calc_error():
+    with pytest.raises(ValueError):
+        factorial(-1)
+        
+def test_factorial_cinc():
+    assert factorial(5)==120
+    
+def test_typerror():
+    with pytest.raises(TypeError):
+        factorial("a")
