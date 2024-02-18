c6ee8ea59657ff567e795a45006f596ba0630f86
andreu97 <albors.marques.andreu@gmail.com>
2024-02-18 18:48:40 +0100

    aaa

--- test/test_tdd.py
+++ test/test_tdd.py
@@ -1,19 +1,20 @@
 import pytest
-
 from practiques.practica4.ci_factorial.src.tdd import factorial
 
 
-
 def test_tdd():
-    assert factorial(1)==1
-    
+    assert factorial(1) == 1
+
+
 def test_calc_error():
     with pytest.raises(ValueError):
         factorial(-1)
-        
+
+
 def test_factorial_cinc():
-    assert factorial(5)==120
-    
+    assert factorial(5) == 120
+
+
 def test_typerror():
     with pytest.raises(TypeError):
         factorial("a")
